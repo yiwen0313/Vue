0c699e8e4805505c61f1792b1a533e663d1947e0
yiwen0313 <2469119402@qq.com>
2021-10-12 21:08:32 +0800

    购物车案例-页面搭建 练习完成

---
+++ CODE/08-书籍购物车案例/main.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        id: 1,
+        name: '《算法导论》',
+        data: '2006-9',
+        price: 85.00,
+        counter:1
+      },
+      {
+        id: 2,
+        name: '《UNIX编程艺术》',
+        data: '2006-2',
+        price: 59.00,
+        counter:1
+      },
+      {
+        id: 3,
+        name: '《编程珠玑》',
+        data: '2008-10',
+        price: 39.00,
+        counter:1
+      },
+      {
+        id: 4,
+        name: '《代码大全》',
+        data: '2006-3',
+        price: 128.00,
+        counter:1
+      }
+    ]
+  }
+});
