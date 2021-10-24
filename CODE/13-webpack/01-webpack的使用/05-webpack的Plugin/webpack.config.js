6ea2de11e1f9869b7a1507bc0a69367c14a0dd5d
yiwen0313 <2469119402@qq.com>
2021-10-24 15:56:50 +0800

    webpack-UglifyjsWebpackPlugin的使用 学习完成

--- CODE/13-webpack/01-webpack的使用/05-webpack的Plugin/webpack.config.js
+++ CODE/13-webpack/01-webpack的使用/05-webpack的Plugin/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -74,6 +75,7 @@ module.exports = {
     new webpack.BannerPlugin('最终版权归xxx所有'),
     new HtmlWebpackPlugin({
       template: 'index.html'
-    })
+    }),
+    new UglifyjsWebpackPlugin()
   ]
 }
