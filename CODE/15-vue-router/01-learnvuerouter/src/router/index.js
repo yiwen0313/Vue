6fecc6548aaca5924ff87020a185c0e8f6de77c1
yiwen0313 <2469119402@qq.com>
2021-10-26 15:19:24 +0800

    vue-router-路由映射配置和呈现出来 学习完成

--- CODE/15-vue-router/01-learnvuerouter/src/router/index.js
+++ CODE/15-vue-router/01-learnvuerouter/src/router/index.js
@@ -1,13 +1,22 @@
 // 配置路由相关信息
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import Home from '../components/Home'
+import About from '../components/About'
 
 // 1.通过Vue.use(插件)，安装插件
 Vue.use(VueRouter)
 
 // 2.创建路由对象
 const routes = [
-
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  }
 ]
 const router = new VueRouter({
   // 配置路由和组件之间的映射关系
