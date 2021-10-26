dbff69e84a54f60e00a5f3abee4087d210590390
yiwen0313 <2469119402@qq.com>
2021-10-26 14:56:49 +0800

    vue-router安装和配置方式 学习完成

---
+++ CODE/15-vue-router/01-learnvuerouter/src/router/index.js
@@ -0,0 +1,20 @@
+// 配置路由相关信息
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+// 1.通过Vue.use(插件)，安装插件
+Vue.use(VueRouter)
+
+// 2.创建路由对象
+const routes = [
+
+]
+const router = new VueRouter({
+  // 配置路由和组件之间的映射关系
+  routes
+})
+
+// 将router对象传入到Vue实例中
+export default router
+
+
