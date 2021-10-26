2b2cd18e501629e8bd12ad2724e4cce0b0782b22
yiwen0313 <2469119402@qq.com>
2021-10-26 16:46:42 +0800

    vue-router-通过代码跳转路由 学习完成

--- CODE/15-vue-router/01-learnvuerouter/src/App.vue
+++ CODE/15-vue-router/01-learnvuerouter/src/App.vue
@@ -2,15 +2,31 @@
   <div id="app">
 <!--<router-link to="/home" tag="button" replace active-class="active">首页</router-link>-->
 <!--<router-link to="/about" tag="button" replace active-class="active">关于</router-link>-->
-    <router-link to="/home" tag="button" replace >首页</router-link>
-    <router-link to="/about" tag="button" replace>关于</router-link>
+<!--<router-link to="/home" tag="button" replace >首页</router-link>-->
+<!--<router-link to="/about" tag="button" replace>关于</router-link>-->
+
+    <button @click="clickHome">首页</button>
+    <button @click="clickAbout">关于</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    clickHome() {
+      // 通过代码的方式修改路由
+      // this.$router.push('/home')
+      this.$router.replace('/home')
+      // console.log('clickHome');
+    },
+    clickAbout() {
+      // this.$router.push('/about')
+      this.$router.replace('/about')
+      // console.log('clickAbout');
+    }
+  }
 }
 </script>
 
