f9fc71c849d83998ea1daf3504504c3df227b1b6
yiwen0313 <2469119402@qq.com>
2021-10-29 17:28:04 +0800

    tabbar-TabBarItem的颜色动态控制 练习完成

--- CODE/15-vue-router/03-exercise-tabbar/src/App.vue
+++ CODE/15-vue-router/03-exercise-tabbar/src/App.vue
@@ -2,22 +2,22 @@
   <div id="app">
     <router-view></router-view>
     <tab-bar>
-      <tab-bar-item path="/home">
+      <tab-bar-item path="/home" activeColor="#ff5777">
         <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
-      <tab-bar-item path="/category">
+      <tab-bar-item path="/category" activeColor="#ff5777">
         <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
-      <tab-bar-item path="/shopcart">
+      <tab-bar-item path="/shopcart" activeColor="#ff5777">
         <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
-      <tab-bar-item path="/profile">
+      <tab-bar-item path="/profile" activeColor="#ff5777">
         <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
         <img slot="item-active-icon" src="./assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
--- CODE/15-vue-router/03-exercise-tabbar/src/components/tabbar/TabBarItem.vue
+++ CODE/15-vue-router/03-exercise-tabbar/src/components/tabbar/TabBarItem.vue
@@ -2,7 +2,7 @@
   <div class="tab-bar-item" @click="itemClick">
     <div v-if="!isActive"><slot name="item-icon"></slot></div>
     <div v-else><slot name="item-active-icon"></slot></div>
-    <div :class="{active: isActive}"><slot name="item-text"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
   </div>
 </template>
 
@@ -10,11 +10,23 @@
 export default {
   name: "TabBarItem",
   props: {
-    path: String
+    path: String,
+    activeColor: {
+      type: String,
+      default: "#000"
+    }
   },
   data() {
     return {
-      isActive: true
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
     }
   },
   methods: {
