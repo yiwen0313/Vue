58e52e8a9a78182458eaebd2875f60a8631d819e
yiwen0313 <2469119402@qq.com>
2021-10-31 20:27:10 +0800

    vuex-mutation的使用 学习完成

---
+++ CODE/16-vuex/01-learnvuex/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h2>-----------APP内容----------</h2>
+    <h2>{{message}}</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="addtion">+</button>
+    <button @click="subtraction">-</button>
+
+    <h2>-----------HelloVuex内容----------</h2>
+    <hello-vuex/>
+  </div>
+</template>
+
+<script>
+import HelloVuex from "./components/HelloVuex";
+
+export default {
+  name: 'App',
+  components: {
+    HelloVuex
+  },
+  data() {
+    return {
+      message: '我是APP组件'
+    }
+  },
+  methods: {
+    addtion() {
+      this.$store.commit('increment')
+    },
+    subtraction() {
+      this.$store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ CODE/16-vuex/01-learnvuex/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// 1.安装插件
+Vue.use(Vuex)
+
+// 2.创建对象
+const store = new Vuex.Store({
+  state: {
+    counter: 1000
+  },
+  mutations: {
+    //方法
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      state.counter--
+    }
+  },
+  actions: {
+
+  },
+  getters: {
+  }
+})
+
+// 3.导出store对象
+export default store
