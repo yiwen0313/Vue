69d76f8998ee80f297f3a1978c59ff9ea55869c0
yiwen0313 <2469119402@qq.com>
2021-11-03 21:09:11 +0800

    axios的拦截器的使用 学习完成

--- CODE/00-axios/01-learnaxios/src/network/request.js
+++ CODE/00-axios/01-learnaxios/src/network/request.js
@@ -7,7 +7,29 @@ export function request(config) {
     timeout: 5000
   })
 
-  // 2.发送真正的网略请求
+  // 2.axios的拦截器
+  // 2.1 请求拦截的作用
+  instance.interceptors.request.use(config => {
+    // console.log(config);
+    // 1)比如config中的一些信息不符合服务器的要求
+
+    // 2)比如每次发送网络请求时，希望在界面中显示一个请求的图标
+
+    // 3)某些网络请求(比如登录(token))，必须携带一些特殊的信息
+    return config
+  }, err => {
+    console.log(err);
+  })
+
+  // 2.2 响应拦截
+  instance.interceptors.response.use(res => {
+    console.log(res);
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  // 3.发送真正的网略请求
   // 本身返回的就是Promise
   return instance(config)
 }
@@ -41,7 +63,7 @@ export function request(config) {
     timeout: 5000
   })
 
-  // 2.发送真正的网略请求
+  // 3.发送真正的网略请求
   instance(config)
       .then(res => {
         resolve(res)
@@ -60,7 +82,7 @@ export function request(config, success, failure) {
     timeout: 5000
   })
 
-  // 2.发送真正的网略请求
+  // 3.发送真正的网略请求
   instance(config)
       .then(res => {
         resolve(res)
